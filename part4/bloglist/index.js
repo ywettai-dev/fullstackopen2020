de5d19fab8c11f7852a0c7d59cea19ad5d4f18f7
Tai Tai <ywettai.dev@gmail.com>
2020-11-18 19:20:31 +0630

    set the blogSchema into its own component

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -4,16 +4,8 @@ const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const Blog = require('./models/blog')
 const mongoose = require('mongoose')
-
-const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 const url = process.env.MONGODB_URI
 
---
+++ part4/bloglist/models/blog.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        minlength: 8,
+        required: true
+    },
+    url: {
+        type: String,
+        minlength: 5
+    },
+    likes: {
+        type: Number
+    }
+})
+
+blogSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
