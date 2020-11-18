0acf0f42b597e9aa7025c085dd26a977177d600a
Tai Tai <ywettai.dev@gmail.com>
2020-11-18 15:42:54 +0630

    add code references from the question && setup the working project

---
+++ part4/bloglist/index.js
@@ -0,0 +1,46 @@
+require('dotenv').config()
+const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const url = process.env.MONGODB_URI
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (req, res) => {
+    Blog
+    .find({})
+    .then(blogs => {
+        res.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (req, res) => {
+    const blog = new Blog(req.body)
+
+    blog
+    .save()
+    .then(result => {
+        res.status(201).json(result)
+    })
+})
+
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+    console.log(`Server running on PORT ${PORT}`)
+})
