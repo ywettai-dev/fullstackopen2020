d72afb750b9719712cf4e944c1561a269da1868f
Tai Tai <ywettai.dev@gmail.com>
2020-11-19 11:35:23 +0630

    finish 4.4 totalLikes list helper

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,8 +1,18 @@
 /* eslint-disable no-unused-vars */
+// dummy test case
 const dummy = (blogs) => {
     return 1
 }
 
+// total likes test case
+const totalLikes = (blogs) => {
+    const reducer = (sum, { likes } ) => {
+        return sum + likes
+    }
+    return blogs.reduce(reducer, 0)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
